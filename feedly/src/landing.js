d7d32430e48ed3f58cd6ac9bd9b5c8bd7a7c7482
DEVU R KURUP <devukurup199918@gmail.com>
2021-10-26 22:38:20 +0530

    Added function to fetch news data for a single category

--- feedly/src/landing.js
+++ feedly/src/landing.js
@@ -2,12 +2,21 @@ import React, {useEffect} from 'react';
 import newsApi from './apis/news';
 
 const Landing = () => {
+
     useEffect(() => {
-        fetchNotes();
+        fetchNews();
       }, []);
+      const fetchNews = async () => {
+        try {
+          const response = await newsApi.fetch();
+          console.log(response)
+        } catch (error) {
+          console.log(error);
+        }
+      };
     return (
         <div>
-            
+           <h1>Hello</h1> 
         </div>
     )
 }
