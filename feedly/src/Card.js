36a976ae548b009419dfbd634d25522a41806122
DEVU R KURUP <devukurup199918@gmail.com>
2021-10-27 22:56:19 +0530

    added main news components

--- feedly/src/Card.js
+++ feedly/src/Card.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Header } from "@bigbinary/neetoui/v2/layouts";
 import { Typography } from "@bigbinary/neetoui/v2";
+import { Button } from "@bigbinary/neetoui/v2";
 
 
 const Card = ({news}) => {
@@ -10,26 +11,45 @@ const Card = ({news}) => {
             <div className="pl-20 pr-20">
               <Header title={<Typography style="h2">Science News</Typography>} /> 
                
-              <div className="grid grid-cols-2">
-                  <div>
-                      <img className=" object-fill h-auto w-auto" src={'https://picsum.photos/seed/picsum/526/263'} />
-                    </div>
-                  <div >
-                      <div>
-                      <Typography style="h3" className= "text-gray-600 text-justify"> {n.title} </Typography>
-                      </div>
-                      <div>
-                      <Typography  style="body3" className="text-gray-400 text-right" >
-                        {n.author + " at " + n.time + " on " + n.date}
-                    </Typography>
-                      </div>
-                      
-                  </div>
-              </div>
+                <div className="grid grid-cols-2">
 
-              </div>
+                    <div>
+                        <img className=" object-fill h-auto w-auto" src={'https://picsum.photos/seed/picsum/526/263'} />
+                    </div>
+
+                    <div>
+
+                        <div>
+                            <Typography style="h3" className= "text-gray-600 text-justify"> {n.title} </Typography>
+                        </div>
+
+                        <div>
+                            <Typography  style="body3" className="text-gray-400 text-right" >
+                                {n.author + " at " + n.time + " on " + n.date}
+                            </Typography>
+                        </div>
+
+                        <div>
+                            <Typography style="body2">
+                                {n.content.substring(0,300) + "..."}    
+                            </Typography>    
+                        </div>
+
+                        <div>
+                            <Button
+                                label="Read more"
+                                onClick={() => alert('read more')}
+                                style="link"
+                            />
+                        </div>
+                      
+                    </div>
+
+                </div>
+
+            </div>
 ))}
-       </div>
+        </div>
     )
 }
 
