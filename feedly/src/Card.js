734727d9b8567d07ae1b95c9aba5361e138dda0b
DEVU R KURUP <devukurup199918@gmail.com>
2021-10-27 23:31:17 +0530

    single category news card done without styling

--- feedly/src/Card.js
+++ feedly/src/Card.js
@@ -6,9 +6,9 @@ import { Button } from "@bigbinary/neetoui/v2";
 
 const Card = ({news}) => {
     return (
-        <div>
-            { news.map( n => (
-            <div className="pl-20 pr-20">
+        <div className="pl-20 pr-20">
+            {/* { news.map( n => (
+            <div >
               <Header title={<Typography style="h2">Science News</Typography>} /> 
                
                 <div className="grid grid-cols-2">
@@ -46,9 +46,148 @@ const Card = ({news}) => {
                     </div>
 
                 </div>
+                <hr className="mt-8"/>
 
             </div>
-))}
+))} */}
+<div >
+              <Header title={<Typography style="h2">Science News</Typography>} /> 
+               
+                <div className="grid grid-cols-2">
+
+                    <div>
+                        <img className=" object-fill h-auto w-auto" src={'https://picsum.photos/seed/picsum/526/263'} />
+                    </div>
+
+                    <div>
+
+                        <div>
+                            <Typography style="h3" className= "text-gray-600 text-justify"> {news[0].title} </Typography>
+                        </div>
+
+                        <div className="pt-2">
+                            <Typography  style="body3" className="text-gray-400 text-right" >
+                                {news[0].author + " at " + news[0].time + " on " + news[0].date}
+                            </Typography>
+                        </div>
+
+                        <div className="pt-5">
+                            <Typography style="body2">
+                                {news[0].content.substring(0,300) + "......"}    
+                            </Typography>    
+                        </div>
+
+                        <div className="pt-4">
+                            <Button
+                                label="Read more"
+                                onClick={() => alert('read more')}
+                                style="link"
+                            />
+                        </div>
+                      
+                    </div>
+
+                </div>
+                <hr className="mt-8 mb-8"/>
+
+            </div>
+       
+        <div className=" grid grid-rows-2">
+            <div className="grid grid-cols-2">
+            <div className="grid grid-cols-2">
+                <div>
+                    <img className=" object-fill h-auto w-auto" src={'https://picsum.photos/seed/picsum/84/84'} />
+                </div>
+                <div>
+                    <div>
+                    <Typography style="h6" className= "text-left"> {news[1].title} </Typography>
+                    </div>
+                    <div className="pt-1">
+                            <Typography  style="body3" className="text-gray-400 text-left" >
+                                {news[1].author + " at " + news[1].time + " on " + news[1].date}
+                            </Typography>
+                    </div>
+                    <div className="pt-1">
+                    <Button
+                                label="Read more"
+                                onClick={() => alert('read more')}
+                                style="link"
+                            />
+                    </div>
+                </div>
+            </div>
+            <div className="grid grid-cols-2">
+                <div>
+                    <img className=" object-fill h-auto w-auto" src={'https://picsum.photos/seed/picsum/84/84'} />
+                </div>
+                <div>
+                    <div>
+                    <Typography style="h6" className= "text-left"> {news[1].title} </Typography>
+                    </div>
+                    <div className="pt-1">
+                            <Typography  style="body3" className="text-gray-400 text-left" >
+                                {news[1].author + " at " + news[1].time + " on " + news[1].date}
+                            </Typography>
+                    </div>
+                    <div className="pt-1">
+                    <Button
+                                label="Read more"
+                                onClick={() => alert('read more')}
+                                style="link"
+                            />
+                    </div>
+                </div>
+            </div>
+            </div>
+            <div className="grid grid-cols-2">
+            <div className="grid grid-cols-2">
+                <div>
+                    <img className=" object-fill h-auto w-auto" src={'https://picsum.photos/seed/picsum/84/84'} />
+                </div>
+                <div>
+                    <div>
+                    <Typography style="h6" className= "text-left"> {news[1].title} </Typography>
+                    </div>
+                    <div className="pt-1">
+                            <Typography  style="body3" className="text-gray-400 text-left" >
+                                {news[1].author + " at " + news[1].time + " on " + news[1].date}
+                            </Typography>
+                    </div>
+                    <div className="pt-1">
+                    <Button
+                                label="Read more"
+                                onClick={() => alert('read more')}
+                                style="link"
+                            />
+                    </div>
+                </div>
+            </div>
+            <div className="grid grid-cols-2">
+                <div>
+                    <img className=" object-fill h-auto w-auto" src={'https://picsum.photos/seed/picsum/84/84'} />
+                </div>
+                <div>
+                    <div>
+                    <Typography style="h6" className= "text-left"> {news[1].title} </Typography>
+                    </div>
+                    <div className="pt-1">
+                            <Typography  style="body3" className="text-gray-400 text-left" >
+                                {news[1].author + " at " + news[1].time + " on " + news[1].date}
+                            </Typography>
+                    </div>
+                    <div className="pt-1">
+                    <Button
+                                label="Read more"
+                                onClick={() => alert('read more')}
+                                style="link"
+                            />
+                    </div>
+                </div>
+            </div>
+            </div>
+            <hr className="mt-8 mb-8"/>
+            
+        </div>
         </div>
     )
 }
