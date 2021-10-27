634e816805e8f604030aa29ae15f49d7aebae2aa
DEVU R KURUP <devukurup199918@gmail.com>
2021-10-27 21:21:44 +0530

    created card with single news image and title

--- feedly/src/Card.js
+++ feedly/src/Card.js
@@ -1,16 +1,24 @@
-import React from 'react'
+import React from 'react';
+import { Header } from "@bigbinary/neetoui/v2/layouts";
 
 const Card = ({news}) => {
     return (
         <div>
-            
-            {news.map(n => (
+            { news.map( n => (
                 <div>
-                <h1>{n.author}</h1>
-                <p>{n.content}</p>
-                </div>
-            ))}
-        </div>
+              <Header title="Science News" /> 
+               
+              <div className="grid grid-cols-2 gap-1">
+                  <div>
+                      <img className=" object-fill h-48 w-9/12" src={n.imageUrl} />
+                  </div>
+                  <div>
+                      <p>{n.title}</p>
+                  </div>
+              </div>
+              </div>
+))}
+       </div>
     )
 }
 
