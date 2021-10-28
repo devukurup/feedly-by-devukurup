26cdb1cd215af5e2cab970b15c9be824862b9881
DEVU R KURUP <devukurup199918@gmail.com>
2021-10-28 19:28:27 +0530

    properly aligned navbar icons

--- feedly/src/Navbar.js
+++ feedly/src/Navbar.js
@@ -1,22 +1,26 @@
 import React from 'react';
 import { Header } from "@bigbinary/neetoui/v2/layouts";
-import { Button } from "@bigbinary/neetoui/v2";
+import { Button,Typography } from "@bigbinary/neetoui/v2";
 import { Search, Filter, Notification } from "@bigbinary/neeto-icons"
 
 const Navbar = () => {
     return (
-        <div className="flex pl-2 pr-2 neeto-ui-shadow-xs">
+        <div className="flex pl-2 pr-2 items-baseline neeto-ui-shadow-xs">
             <Header 
                 actionBlock={
-                    <div className="flex space-x-3 ">
-                        <Search color="#1e1e20" size={18} />
-                        <Notification color="#1e1e20" size={18} />
-                        <div className="flex">
+                    <div className=" flex items-end space-x-3 ">
+                        <div>
+                        <Search color="#1e1e20" size={25} />
+                        </div>
+                        <div>
+                        <Notification color="#1e1e20" size={25} />
+                        </div>
+                        <div >
                             <Button label="Filter" icon={Filter} style="secondary"  />
                         </div>
                     </div>
                     } 
-            title="Feed.ly"/>
+            title={<Typography style="h4" className="text-grey-700">Feed.ly</Typography>}/>
         </div>
     );
 }
