671fb8a8bce4568b9d56190881becb9a79f3a83c
DEVU R KURUP <devukurup199918@gmail.com>
2021-10-28 19:56:10 +0530

    Added tooltip feature

--- feedly/src/Navbar.js
+++ feedly/src/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Header } from "@bigbinary/neetoui/v2/layouts";
-import { Button,Typography } from "@bigbinary/neetoui/v2";
+import { Button,Typography, Tooltip } from "@bigbinary/neetoui/v2";
 import { Search, Filter, Notification } from "@bigbinary/neeto-icons"
 
 const Navbar = () => {
@@ -9,18 +9,21 @@ const Navbar = () => {
             <Header 
                 actionBlock={
                     <div className=" flex items-end space-x-3 ">
-                        <div>
-                        <Search color="#1e1e20" size={25} />
-                        </div>
-                        <div>
-                        <Notification color="#1e1e20" size={25} />
-                        </div>
+                        <Tooltip content="Search" placement="bottom-end">
+                            <button><Search color="#1e1e20" size={25} /></button>
+                        </Tooltip>
+
+                        <Tooltip content="Subscribe" placement="bottom-end" >
+                            <button><Notification color="#1e1e20" size={25} /></button>
+                        
+                        </Tooltip>
+
                         <div >
                             <Button label="Filter" icon={Filter} style="secondary"  />
                         </div>
                     </div>
                     } 
-            title={<Typography style="h4" className="text-grey-700">Feed.ly</Typography>}/>
+            title={<Typography style="h3" className="text-grey-700">Feed.ly</Typography>}/>
         </div>
     );
 }
