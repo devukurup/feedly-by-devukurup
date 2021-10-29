9423352f5c6a10c99c835b173d5c152c127c2865
DEVU R KURUP <devukurup199918@gmail.com>
2021-10-29 23:03:08 +0530

    added tag code in landing

--- feedly/src/App.js
+++ feedly/src/App.js
@@ -1,14 +1,13 @@
 import React, {useState} from 'react';
-import { Tag } from "@bigbinary/neetoui/v2";
 import Navbar from './Navbar';
 import Landing from './Landing';
 import filterContext from './contexts/filter';
 import { categories } from './category';
 
+
 const App = () => {
   const [categoryList, setCategoryList] = useState(['business','national', 'sports','world'])
   const [checkedState, setCheckedState] = useState(categories.map(({id}) => ( categoryList.includes(id) )));
-  const [tag, setTag] = useState(false)
 
   const updateState = (updatedCheckedState) => {
     setCheckedState(updatedCheckedState)
@@ -22,23 +21,16 @@ const App = () => {
       
   }
 
-  const handleClose = (name) => {
-    console.log("im here now")
-    setCategoryList(categoryList.filter(n => n!==name))
-  }
   const newsFeedFilter = {checkedState, setCheckedState, categoryList, setCategoryList, updateState};
+
   return (
     <div>
     <filterContext.Provider value={newsFeedFilter}>
     
       <Navbar />
-      <div className="pt-10 pl-40 pr-40 space-x-3">
-        {console.log(categoryList)}
-      {
-      categoryList.map(( name ) => {
-              return(<Tag label={name.charAt(0).toUpperCase()+name.slice(1)}  onClose={() => handleClose(name)}/>)
-            })}
-      </div>
+
+
+
       <Landing />
       
 
