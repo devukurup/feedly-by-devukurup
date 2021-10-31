a40ce867cc609d270522afa369fc2a756d6cbdcc
DEVU R KURUP <devukurup199918@gmail.com>
2021-10-31 20:53:52 +0530

    Added error boundary template

--- feedly/src/ErrorBoundary/index.js
+++ feedly/src/ErrorBoundary/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import imag from '../assets/error.svg';
+import { Button, Typography } from "@bigbinary/neetoui/v2";
+import { Home } from "@bigbinary/neeto-icons"
+
 
 const ErrorBoundary = () => {
     return (
-        <div>
-<img className="w-auto mx-auto" src={imag} alt="Error Boundary Image" />
+        <div className="flex flex-col justify-center items-center space-y-5 pt-40">
+            <img className="w-auto mx-auto" src={imag} alt="Error Boundary Image" />
+            <div>
+            <Typography style="h3" className="text-center">You have landed somewhere</Typography>
+            <Typography style="h3" className="text-center">unknown.</Typography>
+            </div>
+
+            <Button iconPosition="left" icon={Home} style="secondary" label="Take me home"/>
             
         </div>
     )
